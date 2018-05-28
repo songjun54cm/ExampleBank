133b0e9db1134fce36a7ad8fd97c2376286acc62
unknown <songjun@cn.net.ntes>
2018-05-28 12:23:54 +0800

    nos

--- NOS_example/NOS_PY/NOS_get.py
+++ NOS_example/NOS_PY/NOS_get.py
@@ -8,64 +8,117 @@ import nos
 import json
 from NOS_PY.settings import NOS_ACCESS_KEY, NOS_ENDPOINT, NOS_ACCESS_SECRET, NOS_BUCKET, NOS_OBJECT_KEY_PREFIX
 
-client = nos.Client(
-    access_key_id=NOS_ACCESS_KEY,
-    access_key_secret=NOS_ACCESS_SECRET,
-    end_point=NOS_ENDPOINT,
-)
-# song_id = "songjun_test_song"
-song_id = "472804924"
-bucket = NOS_BUCKET
-object_key = NOS_OBJECT_KEY_PREFIX + song_id
-try:
+
+def main_2():
+    import struct
+    client = nos.Client(
+        access_key_id=NOS_ACCESS_KEY,
+        access_key_secret=NOS_ACCESS_SECRET,
+        end_point=NOS_ENDPOINT,
+    )
+    song_id = "songjun_test_song_1"
+    bucket = NOS_BUCKET
+    object_key = "cnn_feature_" + song_id
     resp = client.get_object(
         bucket=bucket,
         key=object_key,
     )
-    print(resp)
     body = resp['body']
-    feature_info = json.loads(body.read())
-    print("Bucket: %s, Key: %s" % (bucket, object_key))
-    for key in feature_info.keys():
-        val = feature_info[key]
-        if isinstance(val, list):
-            first_val = val[0]
-            last_val = val[-1]
-            size_str = str(len(val))
-            if isinstance(first_val, list):
-                size_str += " * " + str(len(first_val))
-                first_val = first_val[0]
-                last_val = last_val[-1]
-            val_str = "size: <%s>, first: %s, last: %s" % (size_str, str(first_val), str(last_val))
-        else:
-            val_str = str(val)
+    spos = 0
+    epos = 4
+    ndim = struct.unpack('i', body[spos:epos])
+    spos = epos
+    epos = spos + 4*ndim
+    fea_size = struct.unpack('i', body[spos:epos])
+    fea_num = 1
+    for v in fea_size:
+        fea_num *= v
+    spos = epos
+    epos = spos + fea_num
+    feature = struct.unpack('f'*fea_num, body[spos:epos])
+    print(feature[0:10])
 
-        print('key: %s, type: %s, value: %s' % (key, str(type(feature_info[key])), val_str))
+def main_1():
+    client = nos.Client(
+        access_key_id=NOS_ACCESS_KEY,
+        access_key_secret=NOS_ACCESS_SECRET,
+        end_point=NOS_ENDPOINT,
+    )
+    # song_id = "songjun_test_song"
+    song_id = "1002166"
+    key_prefix = "meta_"
+    bucket = NOS_BUCKET
+    object_key = key_prefix + song_id
+    try:
+        resp = client.get_object(
+            bucket=bucket,
+            key=object_key,
+        )
+        print(type(resp))
+        print(resp)
+        body = resp['body']
+        feature_info = json.loads(body.read())
+        print("Bucket: %s, Key: %s" % (bucket, object_key))
+        for key in feature_info.keys():
+            val = feature_info[key]
+            if isinstance(val, list):
+                first_val = val[0]
+                last_val = val[-1]
+                size_str = str(len(val))
+                if isinstance(first_val, list):
+                    size_str += " * " + str(len(first_val))
+                    first_val = first_val[0]
+                    last_val = last_val[-1]
+                val_str = "size: <%s>, first: %s, last: %s" % (size_str, str(first_val), str(last_val))
+            else:
+                val_str = str(val)
 
-except nos.exceptions.ServiceException as e:
-   print (
-       'ServiceException: %s\n'
-       'status_code: %s\n'
-       'error_type: %s\n'
-       'error_code: %s\n'
-       'request_id: %s\n'
-       'message: %s\n'
-   ) % (
-       e,
-       e.status_code,  # error http code
-       e.error_type,   # NOS server error type
-       e.error_code,   # NOS server error code
-       e.request_id,   # request id
-       e.message       # error description message
-   )
-except nos.exceptions.ClientException as e:
-   print (
-       'ClientException: %s\n'
-       'message: %s\n'
-   ) % (
-       e,
-       e.message       # client error message
-   )
+            print('key: %s, type: %s, value: %s' % (key, str(type(feature_info[key])), val_str))
 
-print('get NOS fihish.')
+    except nos.exceptions.ServiceException as e:
+       print (
+           'ServiceException: %s\n'
+           'status_code: %s\n'
+           'error_type: %s\n'
+           'error_code: %s\n'
+           'request_id: %s\n'
+           'message: %s\n'
+       ) % (
+           e,
+           e.status_code,  # error http code
+           e.error_type,   # NOS server error type
+           e.error_code,   # NOS server error code
+           e.request_id,   # request id
+           e.message       # error description message
+       )
+    except nos.exceptions.ClientException as e:
+       print (
+           'ClientException: %s\n'
+           'message: %s\n'
+       ) % (
+           e,
+           e.message       # client error message
+       )
 
+    print('get NOS fihish.')
+
+
+
+if __name__ == '__main__':
+    main_1()
+
+
+
+"""
+main_1:
+song_id = "986433"
+<type 'dict'>
+{'content_length': 135, 'x_nos_request_id': '6cc3c18a-b4fc-4892-8b29-8b63bcd2c9d8', 'body': <urllib3.response.HTTPResponse object at 0x00000000023FEB38>, 'etag': '84d8db3dc0b36ca0ed014c4951ec9be8', 'content_range': '', 'content_type': 'application/octet-stream;charset=UTF-8'}
+Bucket: music-content-feature, Key: feature_986433
+key: url, type: <type 'unicode'>, value: ymusic/b7be/9288/d062/34bc560d94cf6bd26890f724edc56879.mp3
+key: songId, type: <type 'unicode'>, value: 986433
+key: dfsId, type: <type 'NoneType'>, value: None
+key: type, type: <type 'unicode'>, value: NOS
+key: time, type: <type 'int'>, value: 226000
+get NOS fihish.
+"""
--- NOS_example/NOS_PY/NOS_put.py
+++ NOS_example/NOS_PY/NOS_put.py
@@ -10,51 +10,82 @@ import json
 from NOS_PY.settings import NOS_ACCESS_KEY, NOS_ENDPOINT, NOS_ACCESS_SECRET, NOS_BUCKET, NOS_OBJECT_KEY_PREFIX
 
 
-client = nos.Client(
-    access_key_id=NOS_ACCESS_KEY,
-    access_key_secret=NOS_ACCESS_SECRET,
-    end_point=NOS_ENDPOINT,
-)
-
-song_id = "songjun_test_song"
-feature_info = {
-    "audio_id": song_id,
-    "version": "python",
-    "audio_url": "http://url.com",
-    "log_power_mel_feature": np.random.random((10, 3)).tolist(),
-    "cnn_feature": np.random.random(20).tolist()
-}
-
-try:
-    resp = client.put_object(
-        bucket=NOS_BUCKET,
-        key=NOS_OBJECT_KEY_PREFIX + song_id,
-        body=json.dumps(feature_info),
+def main_2():
+    import tempfile
+    import struct
+    client = nos.Client(
+        access_key_id=NOS_ACCESS_KEY,
+        access_key_secret=NOS_ACCESS_SECRET,
+        end_point=NOS_ENDPOINT,
     )
-except nos.exceptions.ServiceException as e:
-   print (
-       'ServiceException: %s\n'
-       'status_code: %s\n'
-       'error_type: %s\n'
-       'error_code: %s\n'
-       'request_id: %s\n'
-       'message: %s\n'
-   ) % (
-       e,
-       e.status_code,  # error http code
-       e.error_type,   # NOS server error type
-       e.error_code,   # NOS server error code
-       e.request_id,   # request id
-       e.message       # error description message
-   )
-except nos.exceptions.ClientException as e:
-   print (
-       'ClientException: %s\n'
-       'message: %s\n'
-   ) % (
-       e,
-       e.message       # client error message
-   )
+    song_id = "songjun_test_song_1"
+    bucket = NOS_BUCKET
+    object_key = "cnn_feature_" + song_id
+    feature = np.random.random(256).flatten().tolist()
+    print(feature[0:10])
+    tempf = tempfile.TemporaryFile('wb')
+    tempf.write(struct.pack('i', 1))
+    tempf.write(struct.pack('i', 256))
+    tempf.write(struct.pack('f'*len(feature), *feature))
 
-print('put NOS fihish.')
+    resp = client.put_object(
+        bucket=bucket,
+        key=object_key,
+        body=tempf
+    )
 
+    print('put NOS fihish.')
+    tempf.close()
+
+def main_1():
+    client = nos.Client(
+        access_key_id=NOS_ACCESS_KEY,
+        access_key_secret=NOS_ACCESS_SECRET,
+        end_point=NOS_ENDPOINT,
+    )
+
+    song_id = "songjun_test_song"
+    feature_info = {
+        "audio_id": song_id,
+        "version": "python",
+        "audio_url": "http://url.com",
+        "log_power_mel_feature": np.random.random((10, 3)).tolist(),
+        "cnn_feature": np.random.random(20).tolist()
+    }
+
+    try:
+        resp = client.put_object(
+            bucket=NOS_BUCKET,
+            key=NOS_OBJECT_KEY_PREFIX + song_id,
+            body=json.dumps(feature_info),
+        )
+    except nos.exceptions.ServiceException as e:
+       print (
+           'ServiceException: %s\n'
+           'status_code: %s\n'
+           'error_type: %s\n'
+           'error_code: %s\n'
+           'request_id: %s\n'
+           'message: %s\n'
+       ) % (
+           e,
+           e.status_code,  # error http code
+           e.error_type,   # NOS server error type
+           e.error_code,   # NOS server error code
+           e.request_id,   # request id
+           e.message       # error description message
+       )
+    except nos.exceptions.ClientException as e:
+       print (
+           'ClientException: %s\n'
+           'message: %s\n'
+       ) % (
+           e,
+           e.message       # client error message
+       )
+
+    print('put NOS fihish.')
+
+
+if __name__ == '__main__':
+    main_2()
