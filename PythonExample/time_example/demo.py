1eb53c4641941f69d29b67bee774c668ffb8bc0a
songjun <songjun@kuaishou.com>
2020-03-04 10:11:23 +0800

    x

--- PythonExample/time_example/demo.py
+++ PythonExample/time_example/demo.py
@@ -14,3 +14,9 @@ d = datetime.datetime.fromtimestamp(timestamp/1000)
 # 精确到毫秒
 str1 = d.strftime("%Y-%m-%d %H:%M:%S.%f")
 print(str1)  # 2019-10-11 14:15:56.514000
+
+# 日期转毫秒
+from datetime import datetime
+timestr = '2019-01-14 15:22:18.123'
+datetime_obj = datetime.strptime(timestr, "%Y-%m-%d %H:%M:%S.%f")
+obj_stamp = int(time.mktime(datetime_obj.timetuple()) * 1000.0 + datetime_obj.microsecond / 1000.0)
