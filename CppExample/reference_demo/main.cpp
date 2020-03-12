23e1e1ad3cbb8f9aab3b92f26625b59492879de2
songjun <songjun54cm@gmail.com>
2020-03-12 09:20:13 +0800

    cpp reference

---
+++ CppExample/reference_demo/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+#include <map>
+class Jun {
+private:
+    std::string name;
+    int age;
+
+public:
+    std::string getName() {
+        return name;
+    }
+    void setName(const std::string& value) {
+        name = value;
+    }
+    int getAge() {
+        return age;
+    }
+    void setAge(int& value) {
+        age = value;
+    }
+    void display() {
+        std::cout << "name: " << name << std::endl
+                  << "age: " << age << std::endl;
+    }
+};
+static std::map<std::string, Jun> name_jun_map;
+
+Jun& get_ref_jun(const std::string& name) {
+    bool flag = (name_jun_map.find(name) == name_jun_map.end());
+    Jun& the_jun = name_jun_map[name];
+    if (flag) {
+        the_jun.setName(name);
+    }
+    return the_jun;
+}
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    std::string name = "song";
+    Jun& the_jun = get_ref_jun((std::string) "song");
+    the_jun.display();
+    return 0;
+}
