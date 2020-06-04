00d11e1d0190edb22c37a4730011be26cbccae6e
songjun <songjun@kuaishou.com>
2020-06-04 17:40:34 +0800

    vector string

--- CppExample/vector_demo/main.cpp
+++ CppExample/vector_demo/main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <map>
 #include <vector>
-int main() {
+#include <string>
+
+void test() {
   std::cout << "Hello, World!" << std::endl;
   std::vector<int> vec{1,2,3,4,5,6,7,8,9,10};
   std::cout << vec.size() << "," << *vec.begin() << "," << *(vec.end()-1)
@@ -11,6 +13,7 @@ int main() {
   std::cout << new_vec.size() << "," << *new_vec.begin() << "," << *(new_vec.end()-1) << std::endl;
 
   std::vector<std::map<int, int>> map_vec(2, std::map<int,int>());
+  // std::vector<std::map<int, int>> map_vec[2]; // error!
 
   map_vec[0][1]=1;
   map_vec[0][12]=12;
@@ -21,8 +24,21 @@ int main() {
 
   std::cout << map_vec[0].size() << std::endl;
   std::cout << map_vec[1].size() << std::endl;
+}
 
+void test1() {
+  std::vector<std::string> str_vec {"aaa",
+                                    "bbb",
+                                    "ccc"};
+  std::cout << "vec size: " << str_vec.size() << std::endl;
+  for (std::string str : str_vec) {
+    std::cout << str << std::endl;
+  }
+}
+
+int main() {
+//  test();
+
+test1();
   return 0;
-
-
 }
