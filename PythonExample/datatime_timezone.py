576421a37e90bbe81c83ccdc27cf5e787d83be77
songjun54cm <songjun54cm@gmail.com>
2025-04-18 10:42:00 +0900

    Create datatime_timezone.py

---
+++ PythonExample/datatime_timezone.py
@@ -0,0 +1,38 @@
+import re
+from datetime import datetime
+import pytz
+
+txt = "20250410  13:43:15    Asia/Tokyo"
+# txt = "20250410  13:43:15    Asia/Tddsf"
+p = re.compile(r"(\d+)\s+(\d\d:\d\d:\d\d)\s+(\S+)")
+x = p.match(txt)
+print(x.group(0))
+
+# print(x.group(1))
+dt = x.group(1)
+print(f"dt = {dt}")
+
+# print(x.group(2))
+hms = x.group(2)
+print(f"hms = {hms}")
+
+# print(x.group(3))
+tzone = x.group(3)
+print(f"tzone = {tzone}")
+
+dt_dt = datetime.strptime(f"{dt} {hms}", "%Y%m%d %H:%M:%S")
+print(f"dt_dt = {dt_dt}")
+
+local_tz = pytz.timezone(tzone)
+
+localtime = dt_dt.astimezone(pytz.timezone(tzone))
+print(f"localtime = {localtime}")
+
+timestamp = int(localtime.timestamp())
+print(f"timestamp = {timestamp}")
+
+jpt = '20250410 13:43:15 Asia/Tokyo'
+dateTime = jpt[0:17]
+timeZone = jpt[18:]
+jptsp = 1744260195
+
